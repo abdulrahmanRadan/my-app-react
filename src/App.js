8ecb65f38e9d3dfd523f609165a28fb8b9d8966c
abdulrahmanRadan <aboda123li123@gmail.com>
2024-11-08 16:15:08 +0300

    update the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+
+import GoalList from "./components/GoalList/GoalList";
+import NewGoal from "./components/NewGoal/NewGoal";
+import TemporaryComponent from "./components/TemporaryComponent/TemporaryComponent";
 
 function App() {
+  const [showOriginal, setShowOriginal] = useState(false);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowOriginal(true);
+    }, 5000); // 5000 milliseconds = 5 seconds
+    return () => clearTimeout(timer); // تنظيف المؤقت عند إلغاء تحميل المكون
+  }, []);
+  const [CourseGoals, setCourseGoal] = useState([
+    { id: "cg1", text: "Finish the Course" },
+    { id: "cg2", text: "Learn all about the Course Main Topic" },
+    { id: "cg3", text: "Help other students in the Course Q&amp;A" },
+  ]);
+
+  const addNewGoalHandler = (newGoal) => {
+    // setCourseGoal(CourseGoals.concat(newGoal));
+    setCourseGoal((prevCourseGoal) => prevCourseGoal.concat(newGoal));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {showOriginal ? (
+        <div className="course-goals">
+          <h2> Course Goals</h2>
+          <NewGoal onAddGoal={addNewGoalHandler} />
+          <GoalList goals={CourseGoals} />
+        </div>
+      ) : (
+        <TemporaryComponent />
+      )}
     </div>
   );
 }
