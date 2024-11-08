dcaeb6a8280e241a4359032f09594e9d3ad2bb82
abdulrahmanRadan <aboda123li123@gmail.com>
2024-11-08 16:14:41 +0300

    create temporary Component

---
+++ src/components/TemporaryComponent/TemporaryComponent.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./TemporaryComponent.css";
+
+const TemporaryComponent = () => {
+  return <div className="loading"></div>;
+};
+
+export default TemporaryComponent;
